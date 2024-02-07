9f43b09ae329fdcaf26a0c04219ba037747eb675
fpata <36186070+fpata@users.noreply.github.com>
2024-02-07 19:17:09 +0530

    Update headerComponent.js

--- components/header/headerComponent.js
+++ components/header/headerComponent.js
@@ -1,19 +1,62 @@
 class HeaderComponent extends HTMLElement {
-    constructor() {
-        super();
-             
-       fetch('./components/header/header.html')
-        .then((response) => {
-          return response.text();
-        })
-        .then((html) => {
-          this.innerHTML = html
-        }); 
-     
-      }
+  constructor() {
+    super();
+    this.render()
+    console.log('Header component initaited');
+  }
 
-      connectedCallback() {
-      }
+  connectedCallback() {
+    console.log("connected callabck called");
+
+  }
+  render() {
+    fetch('./components/header/header.html')
+      .then((response) => {
+        return response.text();
+
+      })
+      .then((html) => {
+        this.innerHTML = html;
+        document.getElementById('btnGlobalSearch').addEventListener('click', this.btnClicked);
+        document.getElementById('navDashBoard').addEventListener('click', this.NaviagteTo);
+        document.getElementById('navAddPatient').addEventListener('click', this.NaviagteTo);
+        document.getElementById('navSearchPatient').addEventListener('click', this.NaviagteTo);
+        document.getElementById('navAppointment').addEventListener('click', this.NaviagteTo);
+        document.getElementById('navReports').addEventListener('click', this.NaviagteTo);
+        document.getElementById('navBilling').addEventListener('click', this.NaviagteTo);
+      })
+  }
+
+  btnClicked() {
+    console.log("button clicked");
+  }
+
+  NaviagteTo() {
+    console.log(this.attributes.id);
+    switch (this.attributes.id.value) {
+      case 'navDashBoard':
+        console.log('dashBoard clicked');
+      break;
+      case 'navAddPatient':
+        console.log('navAddPatient clicked');
+      break;
+      case 'navSearchPatient':
+        console.log('navSearchPatient clicked');
+      break;
+      case 'navAppointment':
+        console.log('navAppointment clicked');
+      break;
+      case 'navReports':
+        console.log('navReports clicked');
+      break;
+      case 'navBilling':
+        console.log('navBilling clicked');
+      break;
+    }
+
+  }
 }
 
+
 window.customElements.define('header-component', HeaderComponent);
+
